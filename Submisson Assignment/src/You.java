8e529d0eaf489a7bd0d611e2e5a8ea0370ae0831
animeisntreal <45776127+animeisntreal@users.noreply.github.com>
2019-06-06 13:21:19 -0400

    I added a constructor, getFullName, setFavoriteAnimal, and happyBirthday methods.
    :D :0

--- Submisson Assignment/src/You.java
+++ Submisson Assignment/src/You.java
@@ -1,6 +1,6 @@
 /**
  * 
- * @author //Put your first and last name here
+ * @author //Harry Allex
  * 
  * 
  * Assignment Instructions:
@@ -18,13 +18,36 @@
  *  - Add achiu-frhsd as a collaborator
  *
  */
-public class You{
+public class You
+{
 
 	private String firstName;
 	private String lastName;
 	private String favoriteAnimal;
 	private int age;
 
-	// Write your code here
+	public You()
+	{
+		firstName = "Harry";
+		lastName = "Allex";
+		favoriteAnimal = "Cats";
+		age = 16;
+	}
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	
+	public String setFavoriteAnimal(String animal)
+	{
+		favoriteAnimal = animal;
+		return favoriteAnimal;
+	}
+	public int happyBirthday()
+	{
+		System.out.println("Happy Birthday! :)");
+		age++;
+		return(age);
+	}
 	
 }
